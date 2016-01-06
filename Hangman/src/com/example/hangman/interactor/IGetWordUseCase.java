3b8bba9068adff41086307f2764bd7076349e964
zhimin115200 <786667916@qq.com>
2016-01-06 16:24:56 +0800

    第二次

--- Hangman/src/com/example/hangman/interactor/IGetWordUseCase.java
+++ Hangman/src/com/example/hangman/interactor/IGetWordUseCase.java
@@ -2,7 +2,6 @@
 package com.example.hangman.interactor;
 
 import com.example.hangman.model.DataSource;
-import com.example.hangman.model.entity.response.StartResponseEntity;
 import com.example.hangman.present.ErrorBundle;
 
 
